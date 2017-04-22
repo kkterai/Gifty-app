cb84aac363e64208e758c1362a5594eed8cb9d6f
kkterai <kkterai@gmail.com>
2017-04-22 13:03:03 -0700

    add erd gem; add foreign keys to store_products

--- db/migrate/20170422194746_create_store_products.rb
+++ db/migrate/20170422194746_create_store_products.rb
@@ -1,7 +1,8 @@
 class CreateStoreProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :store_products do |t|
-
+      t.integer :store_id
+      t.integer :product_id
       t.timestamps
     end
   end
