6b72dcecaf95dc8c66377c2bddc28897a8f6edc9
kkterai <kkterai@gmail.com>
2017-04-22 13:36:24 -0700

    update store and product associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :giver, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
   has_many :store_products
+  has_many :stores, through: :store_products
   #Products are either gifts to be given or items to be shopped for
 end
