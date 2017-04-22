5263dd47f1190a770e2e9f21f74979018e9a1a83
kkterai <kkterai@gmail.com>
2017-04-22 12:49:32 -0700

    add store_products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :store_products
   #Products are either gifts to be given or items to be shopped for
 end
--- db/migrate/20170422194515_create_stores.rb
+++ db/migrate/20170422194515_create_stores.rb
@@ -1,7 +1,7 @@
 class CreateStores < ActiveRecord::Migration[5.0]
   def change
     create_table :stores do |t|
-
+      t.string :name
       t.timestamps
     end
   end
