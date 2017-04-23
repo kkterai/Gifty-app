ffa9f3078d3e99322724f66ec9096209f360a9fb
kkterai <kkterai@gmail.com>
2017-04-23 07:38:56 -0700

    add login and signup abilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   enum role: [:giver, :receiver]
   has_many :lists
-  has_many :products, through: :lists
+  has_many :items, through: :lists
 
   #Products are either desired gifts or products to be shopped for
   has_secure_password
