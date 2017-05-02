b28febadd7cca542a17d6d946a11b393063b0e5a
kkterai <kkterai@gmail.com>
2017-05-02 13:57:44 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  User.create(email: Faker::Internet.safe_email,
+              username: Faker::Internet.user_name,
+              password: Faker::Internet.password(10, 20))
+end
+
+User.all.each do |user|
+  1.times do
+    user.build_list(name: "My Wish List").save
+  end
+end
+
+10.times do
+  Item.create(name: Faker::Commerce.product_name)
+end
