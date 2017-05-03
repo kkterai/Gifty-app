bd5fcb5e56ecaacde346bfe961aa57dacf465261
kkterai <kkterai@gmail.com>
2017-05-02 22:32:45 -0700

    update home view with selection object data

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,7 +1,16 @@
 class SelectionsController < ApplicationController
 
   def create
-    raise params.inspect #"list_item_ids"=>["188", "189"], 
+    list_item_ids = params["list_item_ids"]
+    list_item_ids.each do |li|
+      selection = Selection.new
+      selection.build_user
+      selection.user = current_user
+      selection.build_list_item
+      selection.list_item = ListItem.find_by(id: li)
+      selection.save
+    end
+    redirect_to home_path
   end
 
 end
