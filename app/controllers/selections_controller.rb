2dc01fcd9ea6f10acd1821cb820e042e49d67856
kkterai <kkterai@gmail.com>
2017-05-07 15:56:48 -0700

    undo test sidebar, noted a bug

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -3,18 +3,23 @@ class SelectionsController < ApplicationController
 
   def create
     list_item_ids = params["list_item_ids"]
-    list_item_ids.each do |li|
-      selection = Selection.new
-      selection.build_user
-      selection.user = current_user
-      selection.build_list_item
-      selection.list_item = ListItem.find_by(id: li)
-      selection.save
+    if !list_item_ids == nil
+      list_item_ids.each do |li|
+        selection = Selection.new
+        selection.build_user
+        selection.user = current_user
+        selection.build_list_item
+        selection.list_item = ListItem.find_by(id: li)
+        selection.save
+      end
+      redirect_to home_path
+    else
+      flash[:alert] = "Nothing selected. Please try again"
+      redirect_to users_path
     end
-    redirect_to home_path
   end
 
-  def update 
+  def update
     @selection.list_item.purchased = true
     @selection.list_item.save
     if @selection
