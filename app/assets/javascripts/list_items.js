f30e06b4c436b15bdd8f1b55eb1fd27b1d376a44
kkterai <kkterai@gmail.com>
2017-07-24 14:44:39 -0700

    get AJAX working in list_items.js

--- app/assets/javascripts/list_items.js
+++ app/assets/javascripts/list_items.js
@@ -1,14 +1,29 @@
 $(function() {
     $("#new_item").on("submit", function(e) {
-        // hijack form
-        // fire some ajax
+        // hijack submit event
         //  1. need the URL to submit the POST request to
         url = this.action
-        //  2. we need the form data
-        data = 
-        //  3. send a POST request to the correct place 
+        //  2. we need the form data to send to the server as an AJAX post
         //  (where the form would have submitted anyway)
-        // load that response into the HTML of the page
+       
+        data = {
+            'authenticity_token' : $("input[name = 'authenticity_token']").val(),
+            'item' : {
+                'name' : $("#item_name").val()
+            }
+        }
+        
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            success: function(response) {
+                debugger
+            }
+        })
+        //  3. take the data from the AJAX POST request and create the corresponding list_item
+
+        // 4. Send back HTML/JSON/JS of the list_item that was added and inject it into the OL on the DOM
         e.preventDefault();
     })
 })
