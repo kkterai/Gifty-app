bd5f205f084679843b38a0c2e44b44cc5d2b8322
kkterai <kkterai@gmail.com>
2017-07-27 01:10:11 -0700

    'more' button works

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -4,10 +4,22 @@
 
 $(() => {
   $(".js-more").on('click', function() {
-     $.get(`/list_items/${$(this).data("id")}.json`, function(listItemObject){
-       // console.log(listItemObject)
-       // console.log(listItemObject.comments)
-       console.log(listItemObject.details)
+      var id = $(this).data("id");
+     $.get("/list_items/" + id + ".json", function(listItemObject){
+       var purchased = listItemObject.purchased ? "<p><strong>This Gift is on its way!</strong></p>" : "<p><strong>Not purchased</strong></p>"
+       
+       $("#purchase-" + id).html(purchased);
+       $("#body-" + id).html(listItemObject.details);
+        
+       var comments = listItemObject.comments;
+       var commentList = "";
+
+       comments.forEach(function(comment) {
+        commentList += '<li class="js-comment" data-id="' + comment["id"] + '">' + comment.content + '</li>';
+       });
+
+       $("#list-item-" + id + "-comments").html(commentList);
+
     });
   });
 });
