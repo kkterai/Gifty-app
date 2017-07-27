e45a1f429f4a46e6b32519885b4a355fc96379ee
kkterai <kkterai@gmail.com>
2017-07-27 00:15:16 -0700

    dynamic url achieved in lists.js

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -4,13 +4,12 @@
 
 $(() => {
   $(".js-more").on('click', function() {
-     //dynamically pull out the idea of the More button clicked and append it to the url of the get request below
-     $.get('/list_items/31.json', function(listItemObject){
- 
+     $.get(`/list_items/${$(this).data("id")}.json`, function(listItemObject){
        // console.log(listItemObject)
        // console.log(listItemObject.comments)
        console.log(listItemObject.details)
-    })
+    });
+  });
 });
 
 // Then show each one individually, scroll using "next" link 
