9b62643396b8d0384ab045088a65808130f89a42
kkterai <kkterai@gmail.com>
2017-07-29 12:24:44 -0700

    working on prototype object model for users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,37 @@
 'use strict'
 
+
+function userDescription (){
+    return `<a href='/items/${this.id}'>${this.username}</a>` 
+  }
+
+
+$(() => {
+  $.getJSON('/users', function (data) {
+    var users = $()
+    data.forEach(u => {
+      var user = new User(u.id, u.username)
+      debugger
+      users = users.add(`<p>${user.userDescription()}</p>`)
+    })
+    $('#users').html(users)
+  })
+})
+
+class User {
+  constructor (id, username) {
+    this.id = id
+    this.username = username
+  }
+}
+
+// function capitalize(array) {
+//     var capitalized = "";
+//     debugger
+//     for (var i=0; i < array.length; i++) {
+//         var remainingChars = array[i] ? str.slice(1) : str.slice(1).toLowerCase();
+//         capitalized = capitalized + " " + str.charAt(0).toUpperCase() + remainingChars;
+//         debugger
+//     }
+//  return capitalized;
+// };
