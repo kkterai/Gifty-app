ebb9e4daf99a2d9185c7b60ed1ac357ecc8348a2
kkterai <kkterai@gmail.com>
2017-07-29 15:48:47 -0700

    requirements finished

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,16 +1,5 @@
 'use strict'
 
-$(() => {
-  $.getJSON('/users', function (data) {
-    var users = $()
-    data.forEach(u => {
-      var user = new User(u.id, u.username)
-      users = users.add(`<p>${user.userDescription()}</p>`)
-    })
-    $('#users').html(users)
-  })
-})
-
 class User {
   constructor (id, username) {
     this.id = id
@@ -41,23 +30,34 @@ function capitalize(username) {
  return capitalized;
 };
 
-$(() => {
-function myFunction() {
-    // Declare variables
-    var input, filter, div, p, a, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    div = document.getElementById("#users");
-    p = div.getElementsByTagName('p');
+
+// function search() {
+//     debugger
+//     var input, filter, div, p, a, i;
+//     input = document.getElementById("#search");
+//     filter = input.value.toUpperCase();
+//     div = document.getElementById("#users");
+//     p = div.getElementsByTagName('p');
     
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < p.length; i++) {
-        a = p[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            p[i].style.display = "";
-        } else {
-            p[i].style.display = "none";
-        }
-     }
-   }
-}); 
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < p.length; i++) {
+//         a = p[i].getElementsByTagName("a")[0];
+//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//             p[i].style.display = "";
+//         } else {
+//             p[i].style.display = "none";
+//         }
+//      }
+//    }
+
+$(() => {
+  $.getJSON('/users', function (data) {
+    var users = $()
+    data.forEach(u => {
+      var user = new User(u.id, u.username)
+      users = users.add(`<p>${user.userDescription()}</p>`)
+    })
+    $('#users').html(users)
+  })
+//  $("#search").keyup(search);
+})
